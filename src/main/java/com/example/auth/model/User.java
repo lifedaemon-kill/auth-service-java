fff1b14d9d90764b2b9c5eb6ccb0fe8f2c878689
Ruslan <noname0263@gmail.com>
2024-08-24 16:59:16 +0900

    add model user

---
+++ src/main/java/com/example/auth/model/User.java
@@ -0,0 +1,30 @@
+package com.example.auth.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "ipv4_address")
+    private String ipv4Address;
+
+    @Column(name = "ipv6_address")
+    private String ipv6Address;
+
+}
